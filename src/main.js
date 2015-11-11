6e5978356ba82b4fe29a610707a31a3594765698
Matt McDaniel <matt.h.mcdaniel@gmail.com>
2015-11-10 21:01:29 -0800

    ADD dependencies

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ require("!style!css!sass!../src/styles/main.scss");
 
 import { my_module } from './js/my_module';
 
-var mod = my_module();
+let mod = my_module();
 
-var body = document.querySelector('body');
+let body = document.querySelector('body');
 body.innerHTML += `<h4>Modules loaded: ${mod}</h4>`;
